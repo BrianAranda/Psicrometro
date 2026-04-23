ad45553345f7beab8f6ecb46b58557e3b21be66d
BrianArandaa <brian.aranda.e@gmail.com>
2026-04-23 09:38:42 -0300

    Cambios de nombre e ips

--- src/sistema_wifi.cpp
+++ src/sistema_wifi.cpp
@@ -3,32 +3,29 @@
 
 namespace SistemaWifi {
 
-    // --- Configuraciones de red aislada (SoftAP) ---
-    const char* AP_SSID    = "ESP32-AP";
-    const char* AP_PASS    = "humedades";
-    const int   AP_CHANNEL = 1;
-    const bool  AP_HIDDEN  = false;
-    const int   AP_MAX_CON = 4; // Máximo 4 dispositivos conectados a la vez
+    // Configuraciones de red como Access Point (es una red aislada sin conexión a internet)
+    const char* AP_SSID    = "Proyecto Embebidos";    // Nombre de la red
+    const char* AP_PASS    = "humedades";             // Contraseña de la red 
+    const int   AP_CHANNEL = 1;             // Canal por el que se conecta (entre 1 y 13) 
+    const bool  AP_HIDDEN  = false;         // Para mostrar a todos la red
+    const int   AP_MAX_CON = 4;             // Número máximo de dispositivos conectados a la vez
 
-    // IPs fijas definidas por tu compañero
-    IPAddress local_IP(192, 168, 4, 10);
-    IPAddress gateway(192, 168, 4, 9);
-    IPAddress subnet(255, 255, 255, 0);
+    // IPs fijas para montar el Access Point
+    IPAddress local_IP(10, 10, 10, 10);     // Dirección IPv4 a conectarse luego en la web
+    IPAddress gateway(10, 10, 10, 10);      // Salida a internet por router, formalismo nomas 
+    IPAddress subnet(255, 255, 255, 0);     // Mascara de Red, deja espacio para 254 conexiones
 
     void inicializar() {
-        Serial.println("\n--- Iniciando Sistema de Red ---");
-        Serial.println("Configurando ESP32 como Access Point (Modo Aislado)...");
+        Serial.println("Configurando Access Point ...");
 
-        // Aplicar la configuración de IP estática
+        // Aplicar la configuración IP y levantar el Access Point
         WiFi.softAPConfig(local_IP, gateway, subnet); 
-        
-        // Levantar la red WiFi
         WiFi.softAP(AP_SSID, AP_PASS, AP_CHANNEL, AP_HIDDEN, AP_MAX_CON); 
-
-        Serial.print("Red creada exitosamente. Nombre (SSID): ");
-        Serial.println(AP_SSID);
-        Serial.print("Conectate con el celular e ingresa a la IP: ");
-        Serial.println(WiFi.softAPIP());
-        Serial.println("--------------------------------\n");
+        
+        // Muestra las cosas por serial (todavia no por display)
+        Serial.println("Red creada correctamente:");
+        Serial.print("Nombre (SSID): ");    Serial.println(AP_SSID);
+        Serial.print("Contraseña: ");       Serial.println(AP_PASS);
+        Serial.print("IP del sistema: ");   Serial.println(WiFi.softAPIP());
     }
 }
